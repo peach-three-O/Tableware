d1611e0d2c37ed3ef067311fa666bdaf38d1c832
sonwonjae <sonwj091552@gmail.com>
2023-01-14 21:29:09 +0900

    chore: test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
     root: true,
-    extends: ["next", "turbo", "prettier"],
-    rules: {
-        "@next/next/no-html-link-for-pages": "off",
-        "react/jsx-key": "off",
-    },
+    extends: ["prettier"],
+    rules: {},
     settings: {
         next: {
             rootDir: ["/"],
